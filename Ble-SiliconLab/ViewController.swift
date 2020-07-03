9a22326c0fa9cbfa3a46dcd6c3d34f8ca10a11f5
Amin <amin030296@gmail.com>
2020-07-03 17:00:23 -0500

    Connect disconnect silicon labs

--- Ble-SiliconLab/ViewController.swift
+++ Ble-SiliconLab/ViewController.swift
@@ -11,11 +11,51 @@ import CoreBluetooth
 
 class ViewController: UIViewController {
 
+    var cbCentralManager : CBCentralManager!
+    var peripheral : CBPeripheral?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        cbCentralManager = CBCentralManager(delegate: self, queue: nil)
+        
+        
     }
+}
 
 
+extension ViewController : CBCentralManagerDelegate {
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        
+        if central.state == .poweredOn {
+            central.scanForPeripherals(withServices: nil, options: nil)
+            print("Scanning...")
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        guard peripheral.name != nil else {return}
+        if peripheral.name! == "Thunder Sense #33549" {
+            print("Sensor Found!")
+            print(advertisementData)
+            //stopScan
+            cbCentralManager.stopScan()
+            //connect
+            cbCentralManager.connect(peripheral, options: nil)
+            self.peripheral = peripheral
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print("Connected : \(peripheral.name ?? "No Name")")
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        print("Disconnected : \(peripheral.name ?? "No Name")")
+        cbCentralManager.scanForPeripherals(withServices: nil, options: nil)
+    }
+    
 }
 
